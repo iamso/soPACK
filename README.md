13007ebad3e39f5ebd549ec2fd03dbff67121493
SO <so@sodev.ch>
2013-03-29 18:20:47 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+soPACK
+======
+
+Online Code Compressor powered by [Packer v3.0] and [Ace Editor]
+
+
+Usage
+-----
+
+Paste or write your code into the input editor and click the **PACK** button.
+
+For the right code hinting and highlighting select the language from the dropdown.
+
+That's it! Everything else should be self-explanatory.
+
+
+Features
+--------
+
+* Compress CSS, HTML, Javascript, PHP and XML Code
+* Code Hinting and Highlighting
+* Copy to Clipboard
+
+
+Credits
+-------
+
+soPACK uses some open source projects to work properly:
+
+* [Ace Editor] - awesome web-based text editor
+* [jQuery] - duh
+* [Packer v3.0] - (actually) a Javascript Packer by Dean Edwards
+* [ZeroClipboard] - a jQuery plugin for copying content to clipboard *(uses flash)*
+
+
+License
+-------
+
+MIT License
+
+
+Developer
+---------
+
+soPACK is a project of [soDEV.ch].
+
+  [Ace Editor]: http://ace.ajax.org "Ace Editor"
+  [jQuery]: http://jquery.com
+  [Packer v3.0]: http://dean.edwards.name/packer/ "Packer v3.0 by Dean Edwards"
+  [ZeroClipboard]: http://www.steamdev.com/zclip/
+  [soDEV.ch]: http://soDEV.ch "Check out soDEV.ch"
---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+//@codekit-prepend 'jquery.js';
+//@codekit-prepend 'jquery.zclip.min.js';
+//@codekit-prepend 'my.js';
+//@codekit-prepend 'Packer.js';
+//@codekit-prepend 'Words.js';
+
+var input = ace.edit("input_div");
+input.setTheme("ace/theme/chrome");
+input.getSession().setMode("ace/mode/javascript");
+input.getSession().setTabSize(2);
+document.getElementById('input_div').style.fontSize='12px';
+input.setShowPrintMargin(false);
+input.resize();     
+
+var output = ace.edit("output_div");
+output.setTheme("ace/theme/chrome");
+output.getSession().setMode("ace/mode/javascript");
+output.getSession().setTabSize(2);
+document.getElementById('output_div').style.fontSize='12px';
+output.setShowPrintMargin(false);
+output.renderer.setShowGutter(false);
+output.session.setUseWrapMode(true);
+output.session.setWrapLimitRange();
+output.setHighlightActiveLine(false);
+output.setReadOnly(true);
+output.resize();
+      
+      
+$(function(){
+  $('.ace_text-input').eq(0).attr('id', 'input').attr('name', 'input');
+  $('.ace_text-input').eq(1).attr('id', 'output').attr('name', 'output').attr('disabled', 'disabled').attr('readonly', 'readonly');
+ 
+  $('#clear-all').click(function(){
+    input.getSession().setValue('');
+    output.getSession().setValue('');
+    $('#message').html('ready');
+    $('body').append('<div id="status" class="success">cleared</div>');
+    $('#status').fadeOut(800, 'swing', function(){$(this).remove();});
+  });
+  var packer = new Packer();
+  
+  input.focus();
+       
+  $('#pack-script').click(function(){
+    if (input.getSession().getValue().length > 0) {
+      output.getSession().setValue(packer.pack(input.getSession().getValue(), false, $('#shrink').is(':checked')));
+      var length = input.getSession().getValue().length;
+      if (!/\r/.test(input.getSession().getValue())) { // mozilla trims carriage returns
+      	length += match(input.getSession().getValue(), /\n/g).length;
+      }
+      var calc = output.getSession().getValue().length + "/" + length;
+      var ratio = (output.getSession().getValue().length / length).toFixed(3);
+      var percent = (ratio * 100).toFixed(1);
+      $('#message').html('compression ratio: '+calc+' = '+percent+'%');
+    }        
+  });  
+  $('#copy').zclip({
+    path:'assets/js/ZeroClipboard.swf',
+    copy:function(){return output.getSession().getValue();},
+    beforeCopy:function(){
+        
+    },
+    afterCopy:function(){
+        $('body').append('<div id="status" class="success">copied to clipboard</div>');
+        $('#status').fadeOut(800, 'swing', function(){$(this).remove();});
+    }
+  });
+  $('#language').change(function(){
+    if ($(this).val() === 'javascript') {
+      $('#shrinklabel').css('visibility', 'visible');
+    }
+    else {
+      $('#shrinklabel').css('visibility', 'hidden').find('#shrink').attr('checked', false);
+    }
+    input.getSession().setMode("ace/mode/"+$(this).val());
+    output.getSession().setMode("ace/mode/"+$(this).val());
+  });
+});
